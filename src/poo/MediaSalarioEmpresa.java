1612a691b61de54e2981f294c8b83e807ae5dacd
Alfredo Saraiva <eng.asf1989@gmail.com>
2015-10-09 14:31:28 -0300

    Primeiro commit do projeto

---
+++ src/poo/MediaSalarioEmpresa.java
@@ -0,0 +1,13 @@
+package poo;
+
+public class MediaSalarioEmpresa {
+
+	public static void main(String[] args) {
+		System.out.println("PROGRAMA PARA CALCULAR A MÉDIA SALARIAL DA EMPRESA");
+		System.out.println("__________________________________________________");
+		System.out.println("Qual a quantidade de funcionários?");
+		
+		
+	}
+
+}
